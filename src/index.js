a14d1c9a5ae5a1241795a2998c2eaff49c332506
liem nguyen <elminguyen@gmail.com>
2024-09-08 16:47:12 +0300

    update left, joined status

--- src/index.js
+++ src/index.js
@@ -13,13 +13,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const users = {};
+
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('chat message', (data) => {
-    io.emit('chat message', data); // send data to all
+  socket.on('new user', userName => {
+    users[socket.id] = userName;
+    socket.broadcast.emit('user connected', userName);
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // send data to all
   });
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    socket.broadcast.emit('user disconnected', users[socket.id])
+    delete users[socket.id];
   });
 });
 
